7bc37171e8ac40eda7d7ee65d363941d1f93231a
garciabuenoh <juanmagbh@gmail.com>
2018-03-14 09:48:42 +0100

    Pointing to eureka for device-management request

--- src/src/main/java/s4c/microservices/GIS/services/external/DeviceManagementService.java
+++ src/src/main/java/s4c/microservices/GIS/services/external/DeviceManagementService.java
@@ -16,7 +16,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import s4c.microservices.GIS.model.external.Device;
 import s4c.microservices.GIS.model.external.Device2;
 
 @Service
